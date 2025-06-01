f2bffff25a1dd9be7130b6b6fefb8a5dbc840632
Einsy <eins.yeager@gmail.com>
2025-06-01 15:55:31 +0400

    extention.js

---
+++ extention.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.activate = function activate(context) {
+  // Your extension's activation code goes here
+};
+
+exports.deactivate = function deactivate() {
+  // Your extension's deactivation code goes here
+};
